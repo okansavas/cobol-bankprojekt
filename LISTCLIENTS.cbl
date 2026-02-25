da8cc7f3339261c770358e35c8966f077ed2e0cc
okansavas <158855291+okansavas@users.noreply.github.com>
2026-02-25 17:07:19 +0300

    Update LISTCLIENTS.cbl

--- LISTCLIENTS.cbl
+++ LISTCLIENTS.cbl
@@ -11,29 +11,31 @@
        FILE SECTION.
        FD KUNDEN-DATEI.
        01 KUNDEN-EINTRAG.
-           05 KUNDEN-NR     PIC 9(5).
-           05 KUNDEN-NAME   PIC X(30).
-           05 KUNDEN-KONTO  PIC 9(7)V99.
+           05 KUNDEN-NR      PIC 9(5).
+           05 KUNDEN-NAME    PIC X(30).
+           05 KUNDEN-KONTO   PIC 9(7)V99.
 
        WORKING-STORAGE SECTION.
-       77 EOF-FLAGGE    PIC X VALUE 'N'.
+       01 EOF-FLAGGE         PIC X VALUE 'N'.
+       01 MASK-KONTO         PIC Z(7).99.
 
        PROCEDURE DIVISION.
        BEGIN.
            OPEN INPUT KUNDEN-DATEI
-           DISPLAY "==== Kundenliste ===="
+           DISPLAY " "
+           DISPLAY "========== KUNDENLISTE =========="
+           DISPLAY "NR     NAME                           GUTHABEN"
+           DISPLAY "----------------------------------------------"
 
            PERFORM UNTIL EOF-FLAGGE = 'Y'
                READ KUNDEN-DATEI
                    AT END
                        MOVE 'Y' TO EOF-FLAGGE
                    NOT AT END
-                       DISPLAY "Nr.:      " KUNDEN-NR
-                       DISPLAY "Name:     " KUNDEN-NAME
-                       DISPLAY "Guthaben: " KUNDEN-KONTO
-                       DISPLAY "-----------------------------"
+                       MOVE KUNDEN-KONTO TO MASK-KONTO
+                       DISPLAY KUNDEN-NR "  " KUNDEN-NAME "  " MASK-KONTO " EUR"
                END-READ
            END-PERFORM
 
            CLOSE KUNDEN-DATEI
-           STOP RUN.
+           GOBACK.
